693f7b1289db699df5bf7713de2050cd3c409f24
Aritra <aritra.2003k@gmail.com>
2026-04-14 18:21:40 +0530

    Added K6 test script to hit post route

---
+++ test.js
@@ -0,0 +1,27 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export const options = {
+    vus: 50,
+    duration: "30s"
+};
+
+export default function(){
+    const res = http.post(
+        "http://localhost:3000/event",
+
+        JSON.stringify({
+            eventName: "TEST_EVENT",
+            timestamp: Date.now(),
+            meta: { "user_stamp": `STM+${ Date.now() }`}
+        }),
+
+        {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    );
+
+    sleep(1);
+}
